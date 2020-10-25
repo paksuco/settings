7a43d1a674313c20167cab1414a491133d5d20b0
Taha Paksu <tpaksu@gmail.com>
2020-10-26 01:54:27 +0300

    style

--- views/components/alert.blade.php
+++ views/components/alert.blade.php
@@ -1,5 +1,6 @@
 <div class="text-left mb-4 text-sm relative" x-data="{open: true}" x-show="open">
-    <div class="flex items-center bg-{{$color}}-500 text-{{$textcolor}} text-sm font-semibold rounded px-4 py-3"
+    <div class="flex items-center bg-{{$color}}-100 text-{{$textcolor}} text-sm
+        font-semibold rounded px-4 py-3 border border-{{$color}}-300"
          role="alert">
         <i class="{{$icon}} mr-4 fa-2x leading-none"></i>
         <div>{!! $slot !!}</div>
