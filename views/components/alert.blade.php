ec8f1ece60e88732e79d5229fd4be87ff7f25f74
Taha Paksu <tpaksu@gmail.com>
2020-12-16 09:25:55 +0300

    renaming common flag

--- views/components/alert.blade.php
+++ views/components/alert.blade.php
@@ -1,10 +1,10 @@
-<div class="text-left mb-4 text-sm relative" x-data="{open: true}" x-show="open">
+<div class="relative mb-4 text-sm text-left" x-data="{erroropen: true}" x-show="open">
     <div class="flex items-center bg-{{$color}}-100 text-{{$textcolor}} text-sm
         font-semibold rounded px-4 py-3 border border-{{$color}}-300"
          role="alert">
         <i class="{{$icon}} mr-4 fa-2x leading-none"></i>
         <div>{!! $slot !!}</div>
-        <span class="absolute inset-y-0 right-0 p-4 leading-none cursor-pointer" @click='open = false'>
+        <span class="absolute inset-y-0 right-0 p-4 leading-none cursor-pointer" @click='erroropen = false'>
             <i class="fa fa-times text-{{$textcolor}}"></i>
         </span>
     </div>
