60ed8dea62e44374a852e99051805fb5f9b7a186
Taha Paksu <tpaksu@gmail.com>
2021-01-25 03:59:19 +0300

    Translation Continues

--- views/admin/management.blade.php
+++ views/admin/management.blade.php
@@ -1,58 +1,49 @@
 <div class="flex">
-    <div class="w-full md:w-60 flex-shrink-0 p-6 bg-cool-gray-100 rounded">
-        <h3 class="text-2xl font-semibold mb-3" style="line-height: 1em">@lang("Add new Field")</h3>
+    <div class="flex-shrink-0 w-full p-6 rounded md:w-60 bg-cool-gray-100">
+        <h3 class="mb-3 text-2xl font-semibold" style="line-height: 1em">@lang("Add new Field")</h3>
         <div class='flex flex-wrap -mx-3 text-sm'>
             @livewire('paksuco-settings::editor', [], key('editor'))
-            <div class="px-3 w-full text-right">
+            <div class="w-full px-3 text-right">
                 <button type="button"
-                    class="shadow bg-indigo-500 hover:bg-indigo-400 whitespace-no-wrap focus:shadow-outline
-                        focus:outline-none text-white font-bold py-2 px-3 rounded"
-                    wire:click="addNewField">Save Field</button>
+                    class="px-3 py-2 font-bold text-white whitespace-no-wrap bg-indigo-500 rounded shadow hover:bg-indigo-400 focus:shadow-outline focus:outline-none"
+                    wire:click="addNewField">@lang('Save Field')</button>
             </div>
         </div>
     </div>
     <div class="flex-1 p-6">
-        <h3 class="text-2xl font-semibold mb-3" style="line-height: 1em">@lang("Defined Fields")</h3>
-        <table class="table text-sm border-collapse w-full">
+        <h3 class="mb-3 text-2xl font-semibold" style="line-height: 1em">@lang("Defined Fields")</h3>
+        <table class="table w-full text-sm border-collapse">
             <thead>
                 <tr>
-                    <th class="bg-cool-gray-300 py-1 px-2 text-left font-normal text-cool-gray-800">Field Title
-                    <br>Field Key
-                    </th>
-                    <th class="bg-cool-gray-300 py-1 px-2 text-left font-normal text-cool-gray-800">Field Type
-                    </th>
-                    <th class="bg-cool-gray-300 py-1 px-2 text-left font-normal text-cool-gray-800">Field
-                        Properties</th>
-                    <th class="bg-cool-gray-300 py-1 px-2 text-left font-normal text-cool-gray-800">Actions</th>
+                    <th class="px-2 py-1 font-normal text-left bg-cool-gray-300 text-cool-gray-800">@lang('Field Title')<br>@lang('Field Key')</th>
+                    <th class="px-2 py-1 font-normal text-left bg-cool-gray-300 text-cool-gray-800">@lang('Field Type')</th>
+                    <th class="px-2 py-1 font-normal text-left bg-cool-gray-300 text-cool-gray-800">@lang('Field Properties')</th>
+                    <th class="px-2 py-1 font-normal text-left bg-cool-gray-300 text-cool-gray-800">@lang('Actions')</th>
                 </tr>
             </thead>
             <tbody>
                 @forelse ($options as $option)
                 <tr class="border-cool-gray-300 {{ $loop->last == false ? 'border-b' : '' }}">
-                    <td class="p-2 w-1/3 whitespace-no-wrap"><b class="text-lg font-semibold">
+                    <td class="w-1/3 p-2 whitespace-no-wrap"><b class="text-lg font-semibold">
                         {{$option->field_title}}
                     </b><br>{{$option->field_key}}</td>
-                    <td class="p-2 w-1/3 bg-cool-gray-100 whitespace-no-wrap">{{class_basename($option->field_type)}}</td>
-                    <td class="p-2 w-1/3 whitespace-no-wrap">@dump(json_decode($option->field_properties))</td>
-                    <td class="p-2  bg-cool-gray-100 whitespace-no-wrap">
+                    <td class="w-1/3 p-2 whitespace-no-wrap bg-cool-gray-100">{{class_basename($option->field_type)}}</td>
+                    <td class="w-1/3 p-2 whitespace-no-wrap">@dump(json_decode($option->field_properties))</td>
+                    <td class="p-2 whitespace-no-wrap bg-cool-gray-100">
                         <button
                             type="button"
                             wire:click="edit('{{$option->field_key}}')"
-                            class="shadow bg-blue-700 hover:bg-indigo-400 whitespace-no-wrap focus:shadow-outline
-                            focus:outline-none text-white font-bold py-2 px-3 rounded">
-                            <i class="fa fa-pen mr-2"></i>@lang("Edit")
+                            class="px-3 py-2 font-bold text-white whitespace-no-wrap bg-blue-700 rounded shadow hover:bg-indigo-400 focus:shadow-outline focus:outline-none">
+                            <i class="mr-2 fa fa-pen"></i>@lang("Edit")
                         </button>
                         <button type="button" wire:click="delete('{{$option->field_key}}')"
-                            class="shadow bg-red-700 hover:bg-indigo-400 whitespace-no-wrap focus:shadow-outline
-                            focus:outline-none text-white font-bold py-2 px-3 rounded">
-                            <i class="fa fa-trash-alt mr-2"></i>@lang("Delete")</button>
+                            class="px-3 py-2 font-bold text-white whitespace-no-wrap bg-red-700 rounded shadow hover:bg-indigo-400 focus:shadow-outline focus:outline-none">
+                            <i class="mr-2 fa fa-trash-alt"></i>@lang("Delete")</button>
                     </td>
                 </tr>
                 @empty
                 <tr>
-                    <td colspan="5" class="p-3">There are currently no fields defined. You can define some using the
-                        form below.
-                    </td>
+                    <td colspan="5" class="p-3">@lang('There are currently no fields defined. You can define some using the form below.')</td>
                 </tr>
                 @endforelse
             </tbody>
--- views/fields/repeater/input.blade.php
+++ views/fields/repeater/input.blade.php
@@ -1,29 +1,24 @@
-<div class='w-full md:w-full px-3 mb-4 settings-input'>
-    <table class="bg-white shadow rounded p-4 w-full mb-4">
+<div class='w-full px-3 mb-4 md:w-full settings-input'>
+    <table class="w-full p-4 mb-4 bg-white rounded shadow">
         <thead>
             <tr>
-                <th class="text-left whitespace-no-wrap text-sm uppercase font-semibold p-2 px-4
-                bg-gray-100 sticky top-0 border-b">Key</th>
-                <th class="text-left whitespace-no-wrap text-sm uppercase font-semibold p-2 px-4
-                bg-gray-100 sticky top-0 border-b">Value</th>
-                <th class="text-left whitespace-no-wrap text-sm uppercase font-semibold p-2 px-4
-                bg-gray-100 sticky top-0 border-b">&nbsp;</th>
+                <th class="sticky top-0 p-2 px-4 text-sm font-semibold text-left uppercase whitespace-no-wrap bg-gray-100 border-b">@lang('Key')</th>
+                <th class="sticky top-0 p-2 px-4 text-sm font-semibold text-left uppercase whitespace-no-wrap bg-gray-100 border-b">@lang('Value')</th>
+                <th class="sticky top-0 p-2 px-4 text-sm font-semibold text-left uppercase whitespace-no-wrap bg-gray-100 border-b">&nbsp;</th>
             </tr>
         </thead>
         <tbody>
             @forelse($items as $key => $item)
             <tr class='{{ $loop->even ? "bg-cool-gray-100" : "bg-white" }}''>
-            <td class="border-dashed border-t border-gray-200"><span class="text-gray-700 px-4 py-3 flex items-center whitespace-pre-line">{{$key}}</span></td>
-            <td class="border-dashed border-t border-gray-200"><span class="text-gray-700 px-4 py-3 flex items-center whitespace-pre-line">{{$item}}</span></td>
-            <td class="border-dashed border-t border-gray-200 flex justify-end">
-                <span class="text-gray-700 px-4 py-3 flex items-center whitespace-pre-line">
+            <td class="border-t border-gray-200 border-dashed"><span class="flex items-center px-4 py-3 text-gray-700 whitespace-pre-line">{{$key}}</span></td>
+            <td class="border-t border-gray-200 border-dashed"><span class="flex items-center px-4 py-3 text-gray-700 whitespace-pre-line">{{$item}}</span></td>
+            <td class="flex justify-end border-t border-gray-200 border-dashed">
+                <span class="flex items-center px-4 py-3 text-gray-700 whitespace-pre-line">
                 @if($loop->first == false)
                 <button
                     type="button"
-                    wire:click="moveItemUp(' {{$key}}')"
-                class="btn-primary transition duration-300 ease-in-out focus:outline-none
-                    focus:shadow-outline text-gray-700 hover:text-gray-900
-                    font-normal rounded p-1">
+                    wire:click="moveItemUp('{{$key}}')"
+                class="p-1 font-normal text-gray-700 transition duration-300 ease-in-out rounded btn-primary focus:outline-none focus:shadow-outline hover:text-gray-900">
                 <i class="fa fa-chevron-up"></i>
                 </button>
                 @endif
@@ -31,18 +26,14 @@
                 <button
                     type="button"
                     wire:click="moveItemDown('{{$key}}')"
-                    class="btn-primary transition duration-300 ease-in-out focus:outline-none
-                    focus:shadow-outline text-gray-700 hover:text-gray-900
-                    font-normal rounded p-1">
+                    class="p-1 font-normal text-gray-700 transition duration-300 ease-in-out rounded btn-primary focus:outline-none focus:shadow-outline hover:text-gray-900">
                     <i class="fa fa-chevron-down"></i>
                 </button>
                 @endif
                 <button
                     type="button"
                     wire:click="deleteItem('{{$key}}')"
-                    class="btn-primary transition duration-300 ease-in-out focus:outline-none
-                    focus:shadow-outline text-red-700 hover:text-red-900
-                    font-normal rounded p-1">
+                    class="p-1 font-normal text-red-700 transition duration-300 ease-in-out rounded btn-primary focus:outline-none focus:shadow-outline hover:text-red-900">
                     <i class="far fa-trash-alt"></i>
                 </button>
                 </span>
@@ -50,26 +41,22 @@
             </tr>
             @empty
             <tr>
-                <td colspan="10" class="border-dashed border-t border-gray-200">
-                    <span class="text-gray-700 px-4 py-3 flex items-center whitespace-pre-line">No records found.</span>
+                <td colspan="10" class="border-t border-gray-200 border-dashed">
+                    <span class="flex items-center px-4 py-3 text-gray-700 whitespace-pre-line">@lang('No records found.')</span>
                 </td>
             </tr>
             @endforelse
         </tbody>
     </table>
-    <label class='font-semibold mb-2'>Key</label>
+    <label class='mb-2 font-semibold'>@lang('Key')</label>
     <input wire:model.debounce.500ms='keyInput'
-        class='bg-white border w-full block rounded shadow placeholder-gray-800
-            py-2 px-3 text-gray-700 flex-1 leading-tight focus:border-cool-gray-300
-            border-cool-gray-200 min-w-0 relative text-sm focus:outline-none mb-4'
+        class='relative flex-1 block w-full min-w-0 px-3 py-2 mb-4 text-sm leading-tight text-gray-700 placeholder-gray-800 bg-white border rounded shadow focus:border-cool-gray-300 border-cool-gray-200 focus:outline-none'
         type='text'>
-    <label class='font-semibold mb-2'>Value</label>
+    <label class='mb-2 font-semibold'>@lang('Value')</label>
     <input wire:model.debounce.500ms='valueInput'
-        class='bg-white border w-full block rounded shadow placeholder-gray-800
-            py-2 px-3 text-gray-700 flex-1 leading-tight focus:border-cool-gray-300
-            border-cool-gray-200 min-w-0 relative text-sm focus:outline-none mb-4'
+        class='relative flex-1 block w-full min-w-0 px-3 py-2 mb-4 text-sm leading-tight text-gray-700 placeholder-gray-800 bg-white border rounded shadow focus:border-cool-gray-300 border-cool-gray-200 focus:outline-none'
         type='text'>
     <button type="button"
-        class="transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-indigo-700 hover:bg-indigo-900 text-white font-normal py-2 px-4 mr-1 rounded shadow border-indigo-800"
-        wire:click="addItem">Add Item</button>
+        class="px-4 py-2 mr-1 font-normal text-white transition duration-300 ease-in-out bg-indigo-700 border-indigo-800 rounded shadow focus:outline-none focus:shadow-outline hover:bg-indigo-900"
+        wire:click="addItem">@lang('Add Item')</button>
 </div>
