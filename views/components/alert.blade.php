4696ac9d36222114dec92745843e3c706ae58e6f
Taha Paksu <tpaksu@gmail.com>
2021-06-14 08:31:24 +0300

    focus

--- views/components/alert.blade.php
+++ views/components/alert.blade.php
@@ -1,7 +1,7 @@
-<div class="relative mb-4 text-sm text-left" x-data="{erroropen: true}" @message-processed.window="erroropen = true"
+<div class="relative mb-4 text-sm text-left" x-data="{erroropen: true}" @message-processed.window="erroropen = true" id="error-indicator"
     x-show="erroropen">
     <div class="flex items-center bg-{{$color}}-100 text-{{$textcolor}} text-sm
-        font-semibold rounded px-4 py-3 border border-{{$color}}-300"
+        font-semibold rounded px-4 py-3 border border-{{$color}}-300 pr-10"
         role="alert">
         <i class="{{$icon}} mr-3 mt-1 fa-2x leading-none"></i>
         <div>{!! $slot !!}</div>
